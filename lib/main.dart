9feef0a95c769f42b51cfd858e3baa26b0c57d89
Yasser Hajlaoui <yasser159@gmail.com>
2023-02-21 14:34:39 -0800

    added consts and removed appbar

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp(debugShowCheckedModeBanner: false,
       home: ExampleLayoutBuilder(),
     );
   }
@@ -17,9 +17,7 @@ class ExampleLayoutBuilder extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('LayoutBuilder Example'),
-      ),
+
       body: LayoutBuilder(
         builder: (BuildContext context, BoxConstraints constraints) {
           return SingleChildScrollView(
@@ -29,23 +27,23 @@ class ExampleLayoutBuilder extends StatelessWidget {
                   height: 200,
                   width: constraints.maxWidth,
                   color: Colors.blue,
-                  child: Center(
+                  child: const Center(
                     child: Text(
                       'Header',
                       style: TextStyle(fontSize: 30, color: Colors.white),
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Container(
                   width: constraints.maxWidth,
-                  padding: EdgeInsets.all(20),
-                  child: Text(
+                  padding: const EdgeInsets.all(20),
+                  child: const Text(
                     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
                     style: TextStyle(fontSize: 16),
                   ),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
@@ -66,19 +64,19 @@ class ExampleLayoutBuilder extends StatelessWidget {
                     ),
                   ],
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Container(
                   height: 200,
                   width: constraints.maxWidth,
                   color: Colors.orange,
-                  child: Center(
+                  child: const Center(
                     child: Text(
                       'Footer',
                       style: TextStyle(fontSize: 30, color: Colors.white),
                     ),
                   ),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
               ],
             ),
           );
